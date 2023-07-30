0b56dead8d39161cd8dfec5d397cafd022fcde55
Ian Hartwig <2886013+ihartwig@users.noreply.github.com>
2023-07-29 20:47:12 -0700

    Update conf.py

--- doc/conf.py
+++ doc/conf.py
@@ -103,7 +103,10 @@ html_context = {
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
-html_static_path = []
+html_static_path = ['source/static']
+
+# Extra CSS files for the rtd theme
+html_css_files = ['team5026.css']
 
 # Custom sidebar templates, must be a dictionary that maps document names
 # to template names.
